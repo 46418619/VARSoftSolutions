abadf62d146a70c12bd059ce156584b4057abde0
Vipul Kumar <vipulkumar0719410113@gmail.com>
2023-12-20 10:46:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Footer from "./Layout/Footer";
+import Header from "./Layout/Header";
+import Home from "./Pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
